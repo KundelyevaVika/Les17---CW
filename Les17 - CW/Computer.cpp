f7c7fd326decd4aa21278679d10beb159e28fd83
Kundelyeva Vika <viktoriya.kundelyeva@gmail.com>
2024-01-09 17:51:41 +0200

    definition methods

--- Les17 - CW/Computer.cpp
+++ Les17 - CW/Computer.cpp
@@ -1,1 +1,24 @@
 #include "Computer.h"
+
+Computer::Computer(string name_, float frequency_, int OZU_, bool DVD_, float cost_)
+{
+    name = name_;
+    frequency = frequency_;
+    OZU = OZU_;
+    DVD = DVD_;
+    cost = cost_;
+}
+
+int Computer::getOZU() const
+{
+    return OZU;
+}
+
+void Computer::show() const
+{
+    cout << "Name: " << name << endl;
+    cout << "Frequency: " << frequency << "GHz" << endl;
+    cout << "OZU: " << OZU << endl;
+    cout << "DVD: " << DVD << endl;
+    cout << "Cost: " << cost << "UAH" << endl;
+}
